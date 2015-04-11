a3a9a172434513d5db1cf4bb6c439db9b527825b
provirabosch <pablorovirabosch@gmail.com>
2015-04-11 16:20:43 -0300

    Calendar Icon added

--- app/src/main/java/com/funebunny/xpdroid/main/ui/DatePickerFragment.java
+++ app/src/main/java/com/funebunny/xpdroid/main/ui/DatePickerFragment.java
@@ -5,6 +5,9 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.widget.DatePicker;
+import android.widget.EditText;
+
+import com.funebunny.xpdroid.R;
 
 import java.util.Calendar;
 
@@ -13,13 +16,15 @@ import java.util.Calendar;
  */
 public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
 
+    private int year, month, day;
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the current date as the default date in the picker
         final Calendar c = Calendar.getInstance();
-        int year = c.get(Calendar.YEAR);
-        int month = c.get(Calendar.MONTH);
-        int day = c.get(Calendar.DAY_OF_MONTH);
+        year = c.get(Calendar.YEAR);
+        month = c.get(Calendar.MONTH);
+        day = c.get(Calendar.DAY_OF_MONTH);
 
         // Create a new instance of DatePickerDialog and return it
         return new DatePickerDialog(getActivity(), this, year, month, day);
@@ -28,6 +33,12 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
 
     @Override
     public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+        year = year;
+        month = monthOfYear;
+        day = dayOfMonth;
+
+//        EditText fecha = (EditText) findViewById(R.id.fecha);
 
     }
 }
