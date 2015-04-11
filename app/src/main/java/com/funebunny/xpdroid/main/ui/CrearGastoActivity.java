e21ec3e111afdc6aa9ff2f97c7c134c696f2bb9b
provirabosch <pablorovirabosch@gmail.com>
2015-04-11 16:03:59 -0300

    CrearGasto Activity added

--- app/src/main/java/com/funebunny/xpdroid/main/ui/CrearGastoActivity.java
+++ app/src/main/java/com/funebunny/xpdroid/main/ui/CrearGastoActivity.java
@@ -2,30 +2,19 @@ package com.funebunny.xpdroid.main.ui;
 
 import android.app.DialogFragment;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
 import com.funebunny.xpdroid.R;
 
-
-public class CrearGasto extends ActionBarActivity {
+public class CrearGastoActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_gasto);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
     }
 
 
@@ -51,26 +40,10 @@ public class CrearGasto extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_crear_gasto, container, false);
-            return rootView;
-        }
-    }
-
     //>Added by PRB
     public void mostrarDatePicker(View v) {
-//        DialogFragment newFragment = new DatePickerFragment();
-//        newFragment.show(getFragmentManager(), "datePicker");
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getFragmentManager(), "datePicker");
     }
     //<Added by PRB
 }
