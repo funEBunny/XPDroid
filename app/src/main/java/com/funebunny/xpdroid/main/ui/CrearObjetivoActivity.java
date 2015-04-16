22e022f4ca558bc87a0b8d718b94adee4adfb492
provirabosch <pablorovirabosch@gmail.com>
2015-04-16 16:25:35 -0300

    Crear Objetivo Activity added

---
+++ app/src/main/java/com/funebunny/xpdroid/main/ui/CrearObjetivoActivity.java
@@ -0,0 +1,49 @@
+package com.funebunny.xpdroid.main.ui;
+
+import android.app.DialogFragment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.funebunny.xpdroid.R;
+
+public class CrearObjetivoActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_objetivo);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_crear_objetivo, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //>Added by PRB
+    public void mostrarDatePicker(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getFragmentManager(), "datePicker");
+    }
+    //<Added by PRB
+}
