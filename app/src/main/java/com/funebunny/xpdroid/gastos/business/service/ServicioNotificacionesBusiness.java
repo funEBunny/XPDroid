3936d6d673e3976e585cdd4787b682229cd2ee88
aschmidt7 <alfredo.schmidt@gmail.com>
2015-08-15 16:33:44 -0300

    gasto programable diario y semanal con hora pasada

--- app/src/main/java/com/funebunny/xpdroid/gastos/business/service/ServicioNotificacionesBusiness.java
+++ app/src/main/java/com/funebunny/xpdroid/gastos/business/service/ServicioNotificacionesBusiness.java
@@ -22,26 +22,26 @@ public class ServicioNotificacionesBusiness {
     public static final int MINUTOS = 1;
     public static final String SEPARADOR_HORA = ":";
     public static final String NOTIF_ID = "notifID";
-    public static final long INTERVAL_WEEK = 7*AlarmManager.INTERVAL_DAY;
+    public static final long INTERVAL_WEEK = 7 * AlarmManager.INTERVAL_DAY;
     public static final long INTERVAL_DAY = AlarmManager.INTERVAL_DAY;
 
-    void activarAlarmaDiaria(Context applicationContext, String time, Long id){
-        Calendar calendar = getCalendar(time,0);
+    void activarAlarmaDiaria(Context applicationContext, String time, Long id) {
+        Calendar calendar = getCalendar(time, 0);
         activarAlarma(applicationContext, calendar, id, INTERVAL_DAY);
 
     }
 
-    void activarAlarmaSemanal(Context applicationContext,int day, String time, Long id){
-        Calendar calendar = getCalendar(time,day);
+    void activarAlarmaSemanal(Context applicationContext, int day, String time, Long id) {
+        Calendar calendar = getCalendar(time, day);
         activarAlarma(applicationContext, calendar, id, INTERVAL_WEEK);
 
     }
 
     private void activarAlarma(Context applicationContext, Calendar calendar, Long id, long repeticion) {
         AlarmManager alarm = (AlarmManager) applicationContext.getSystemService(Context.ALARM_SERVICE);
-        Intent myIntent = new Intent(applicationContext, AlarmChecker.class).putExtra(NOTIF_ID,id);
+        Intent myIntent = new Intent(applicationContext, AlarmChecker.class).putExtra(NOTIF_ID, id);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(applicationContext, id.intValue(), myIntent, 0);
-        alarm.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),repeticion, pendingIntent);
+        alarm.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), repeticion, pendingIntent);
     }
 
     private Calendar getCalendar(String time, int dia) {
@@ -50,28 +50,36 @@ public class ServicioNotificacionesBusiness {
         String minutos = splitTime[MINUTOS];
 
         Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(System.currentTimeMillis());
+        int todayDOW = calendar.get(Calendar.DAY_OF_WEEK);
+        if (dia!=0){
+            if (todayDOW==dia){
+                if (esHoraPasada(time)){
+                    calendar.add(Calendar.DAY_OF_WEEK, dia);
+                }
+             }else{
+                calendar.add(Calendar.DAY_OF_WEEK, dia);
+            }
+        }else{
+            if (esHoraPasada(time)){
+                calendar.add(Calendar.DATE,1);
+            }
+        }
         calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(hora));
         calendar.set(Calendar.MINUTE, Integer.parseInt(minutos));
         calendar.set(Calendar.SECOND, ZERO_SEGUNDOS);
-        if (dia!=0&&isFechaFutura(time)){
-            calendar.add(Calendar.DAY_OF_WEEK,dia);
-        }
         return calendar;
     }
 
-    private boolean isFechaFutura(String time) {
-        SimpleDateFormat fromUser = new SimpleDateFormat("HH:mm");
+    private boolean esHoraPasada(String time) {
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HHmm");
         Date today = Calendar.getInstance().getTime();
-        try {
-            Date date = fromUser.parse(time);
-            if (date.after(today)){
-                return true;
-            }else{
-                return false;
-            }
-        } catch (ParseException e) {
-           return false;
+        String todayTime = timeFormat.format(today);
+        String timeToCompare = time.replace(":","");
+
+        if (Integer.valueOf(todayTime)>Integer.valueOf(timeToCompare)) {
+            return true;
+        } else {
+            return false;
         }
     }
 
