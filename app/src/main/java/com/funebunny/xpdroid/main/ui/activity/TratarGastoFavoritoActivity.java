2aca673c524980b5b7ab70fc9baa7c54958b3354
provirabosch <pablorovirabosch@gmail.com>
2015-08-16 19:30:47 -0300

    Back arreglado en Tratar Gastos Favoritos

--- app/src/main/java/com/funebunny/xpdroid/main/ui/activity/TratarGastoFavoritoActivity.java
+++ app/src/main/java/com/funebunny/xpdroid/main/ui/activity/TratarGastoFavoritoActivity.java
@@ -51,14 +51,14 @@ public class TratarGastoFavoritoActivity extends XPDroidActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                overridePendingTransition(R.animator.in_left, R.animator.out_right);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void actualizarGastoFavorito(View v){
