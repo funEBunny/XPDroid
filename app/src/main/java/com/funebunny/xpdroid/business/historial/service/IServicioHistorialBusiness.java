f3055bdd0f89d16603483756760a25d90bc785b4
aschmidt7 <alfredo.schmidt@gmail.com>
2015-09-05 16:13:19 -0300

    Interfaz historial gastos business

--- app/src/main/java/com/funebunny/xpdroid/business/historial/service/IServicioHistorialBusiness.java
+++ app/src/main/java/com/funebunny/xpdroid/business/historial/service/IServicioHistorialBusiness.java
@@ -1,7 +1,19 @@
 package com.funebunny.xpdroid.business.historial.service;
 
+import com.funebunny.xpdroid.business.historial.model.Historial;
+
+import java.util.List;
+
 /**
  * Created by I823537 on 05/09/2015.
  */
 public interface IServicioHistorialBusiness {
+
+    List<Historial> obtenerListaHistorial();
+
+    void guardarHistorial(Historial historial);
+
+    void eliminarHistorial(Long id);
+
+    void actualizarHistorial(Historial historial);
 }
