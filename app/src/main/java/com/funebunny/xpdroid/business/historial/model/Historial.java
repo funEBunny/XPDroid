7edba79f31d9a4e6dc6e3a7692ba39a823881b26
provirabosch <pablorovirabosch@gmail.com>
2015-09-05 14:08:00 -0300

    Clase Historial model

--- app/src/main/java/com/funebunny/xpdroid/business/historial/model/Historial.java
+++ app/src/main/java/com/funebunny/xpdroid/business/historial/model/Historial.java
@@ -4,4 +4,34 @@ package com.funebunny.xpdroid.business.historial.model;
  * Created by I823537 on 05/09/2015.
  */
 public class Historial {
+
+    private String mes;
+    private String anio;
+    private String total;
+
+    public String getMes() {
+        return mes;
+    }
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    public String getAnio() {
+        return anio;
+    }
+
+    public void setAnio(String anio) {
+        this.anio = anio;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+
 }
