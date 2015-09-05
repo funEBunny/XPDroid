3edf2cee2fbeb95ac52dd5b4bcb36579e4b02b77
aschmidt7 <alfredo.schmidt@gmail.com>
2015-09-05 16:27:55 -0300

    implement methods historial bl

--- app/src/main/java/com/funebunny/xpdroid/business/historial/service/ServicioHistorialBusiness.java
+++ app/src/main/java/com/funebunny/xpdroid/business/historial/service/ServicioHistorialBusiness.java
@@ -1,7 +1,30 @@
 package com.funebunny.xpdroid.business.historial.service;
 
+import com.funebunny.xpdroid.business.historial.model.Historial;
+
+import java.util.List;
+
 /**
  * Created by I823537 on 05/09/2015.
  */
 public class ServicioHistorialBusiness implements IServicioHistorialBusiness {
+    @Override
+    public List<Historial> obtenerListaHistorial() {
+        return null;
+    }
+
+    @Override
+    public void guardarHistorial(Historial historial) {
+
+    }
+
+    @Override
+    public void eliminarHistorial(Long id) {
+
+    }
+
+    @Override
+    public void actualizarHistorial(Historial historial) {
+
+    }
 }
