8fa3ea8154b2e53ab50398e27ad2c2b5a5202463
provirabosch <pablorovirabosch@gmail.com>
2015-10-17 12:23:54 -0300

    #30 Test ServicioPresupuestoBusiness

---
+++ app/src/androidTest/java/com/funebunny/xpdroid/business/presupuesto/service/ServicioPresupuestoBusinessTest.java
@@ -0,0 +1,9 @@
+package com.funebunny.xpdroid.business.presupuesto.service;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by provirabosch on 17/10/2015.
+ */
+public class ServicioPresupuestoBusinessTest extends AndroidTestCase {
+}
