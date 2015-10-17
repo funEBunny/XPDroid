3de6d263c5f5e77bbd06bef260da9ca14f5b0c89
provirabosch <pablorovirabosch@gmail.com>
2015-10-17 12:20:35 -0300

    #28 Test ServicioGastosBusiness

---
+++ app/src/androidTest/java/com/funebunny/xpdroid/business/gasto/service/ServicioGastosBusinessTest.java
@@ -0,0 +1,9 @@
+package com.funebunny.xpdroid.business.gasto.service;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by provirabosch on 17/10/2015.
+ */
+public class ServicioGastosBusinessTest extends AndroidTestCase{
+}
