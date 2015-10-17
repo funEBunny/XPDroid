6ca245ba9c1a8afbefce21cc7d4124941637608b
akawakita <adrian.kawakita@gmail.com>
2015-10-17 16:59:17 -0300

    #28 Initial commit

--- app/src/androidTest/java/com/funebunny/xpdroid/business/gasto/service/ServicioGastosBusinessTest.java
+++ app/src/androidTest/java/com/funebunny/xpdroid/business/gasto/service/ServicioGastosBusinessTest.java
@@ -2,8 +2,33 @@ package com.funebunny.xpdroid.business.gasto.service;
 
 import android.test.AndroidTestCase;
 
+import com.funebunny.xpdroid.backend.gasto.service.ServicioGastosDAO;
+import com.funebunny.xpdroid.business.gasto.model.Gasto;
+
 /**
  * Created by provirabosch on 17/10/2015.
  */
 public class ServicioGastosBusinessTest extends AndroidTestCase{
+
+    private ServicioGastosBusiness servicioGastosBusiness;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        servicioGastosBusiness = new ServicioGastosBusiness();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testGuardarGasto() {
+
+        //servicioGastosBusiness.guardarGasto();
+
+    }
+
+
+
 }
