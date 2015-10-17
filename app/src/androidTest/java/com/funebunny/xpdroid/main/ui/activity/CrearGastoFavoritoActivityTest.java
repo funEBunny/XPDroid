62ec0bd6232a5dc1710b446c3e9fef757f1f0b70
provirabosch <pablorovirabosch@gmail.com>
2015-10-17 13:09:16 -0300

    #34 Test CrearGastoFavoritoActivity

---
+++ app/src/androidTest/java/com/funebunny/xpdroid/main/ui/activity/CrearGastoFavoritoActivityTest.java
@@ -0,0 +1,12 @@
+package com.funebunny.xpdroid.main.ui.activity;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by provirabosch on 17/10/2015.
+ */
+public class CrearGastoFavoritoActivityTest extends ActivityInstrumentationTestCase2<CrearGastoFavoritoActivity> {
+    public CrearGastoFavoritoActivityTest() {
+        super(CrearGastoFavoritoActivity.class);
+    }
+}
