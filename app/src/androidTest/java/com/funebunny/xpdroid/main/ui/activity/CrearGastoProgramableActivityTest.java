a767d7c4f24e55b6ded6ae00455965efe46c78ed
provirabosch <pablorovirabosch@gmail.com>
2015-10-17 13:03:59 -0300

    #32 Test CrearGastoProgramableActivity

---
+++ app/src/androidTest/java/com/funebunny/xpdroid/main/ui/activity/CrearGastoProgramableActivityTest.java
@@ -0,0 +1,12 @@
+package com.funebunny.xpdroid.main.ui.activity;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by provirabosch on 17/10/2015.
+ */
+public class CrearGastoProgramableActivityTest extends ActivityInstrumentationTestCase2<CrearGastoProgramableActivity>{
+    public CrearGastoProgramableActivityTest() {
+        super(CrearGastoProgramableActivity.class);
+    }
+}
