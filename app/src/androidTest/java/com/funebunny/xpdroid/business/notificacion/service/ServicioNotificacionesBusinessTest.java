5acfa49268ba45843810eb1bd14dd1d2aaf9f998
provirabosch <pablorovirabosch@gmail.com>
2015-10-17 12:15:58 -0300

    #27 Test Servicio Notificaciones

---
+++ app/src/androidTest/java/com/funebunny/xpdroid/business/notificacion/service/ServicioNotificacionesBusinessTest.java
@@ -0,0 +1,9 @@
+package com.funebunny.xpdroid.business.notificacion.service;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by provirabosch on 17/10/2015.
+ */
+public class ServicioNotificacionesBusinessTest extends AndroidTestCase {
+}
