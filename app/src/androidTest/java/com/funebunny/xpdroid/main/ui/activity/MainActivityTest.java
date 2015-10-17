2715c6a370ff376e67ad8e7b5e8af97fa14a0636
provirabosch <pablorovirabosch@gmail.com>
2015-10-17 12:27:52 -0300

    #31 Test MainActivity

---
+++ app/src/androidTest/java/com/funebunny/xpdroid/main/ui/activity/MainActivityTest.java
@@ -0,0 +1,12 @@
+package com.funebunny.xpdroid.main.ui.activity;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by provirabosch on 17/10/2015.
+ */
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+}
