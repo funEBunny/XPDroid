04113613407e5cbeba56e0956305e310b854703d
provirabosch <pablorovirabosch@gmail.com>
2015-10-20 17:20:25 -0300

    #30

--- app/src/androidTest/java/com/funebunny/xpdroid/business/presupuesto/service/ServicioPresupuestoBusinessTest.java
+++ app/src/androidTest/java/com/funebunny/xpdroid/business/presupuesto/service/ServicioPresupuestoBusinessTest.java
@@ -6,6 +6,8 @@ import com.funebunny.xpdroid.business.presupuesto.model.Presupuesto;
 import com.funebunny.xpdroid.utilities.AppConstants;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by provirabosch on 17/10/2015.
@@ -68,4 +70,49 @@ public class ServicioPresupuestoBusinessTest extends AndroidTestCase {
         servicioPresupuestoBusiness.guardarPresupuesto(AppConstants.PERIODO_DIARIO, "100");
         assertTrue(servicioPresupuestoBusiness.tipoPresupuestoExiste(AppConstants.PERIODO_DIARIO));
     }
+
+    public void testIsMismaSemana_verdadero() {
+        Calendar calendar = Calendar.getInstance();
+        assertTrue(servicioPresupuestoBusiness.isMismaSemana(calendar));
+    }
+
+    public void testIsMismaSemana_falso() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.WEEK_OF_YEAR, 1);
+        assertFalse(servicioPresupuestoBusiness.isMismaSemana(calendar));
+    }
+
+    public void testIsMismoMes_verdadero() {
+        Calendar calendar = Calendar.getInstance();
+        assertTrue(servicioPresupuestoBusiness.isMismoMes(calendar));
+    }
+
+    public void testIsMismoMes_falso() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MONTH, 1);
+        assertFalse(servicioPresupuestoBusiness.isMismoMes(calendar));
+    }
+
+    public void testIsMismoDia_verdadero() {
+        Calendar calendar = Calendar.getInstance();
+        assertTrue(servicioPresupuestoBusiness.isMismoDia(calendar));
+    }
+
+    public void testIsMismoDia_falso() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_YEAR, 1);
+        assertFalse(servicioPresupuestoBusiness.isMismoDia(calendar));
+    }
+
+    public void testIsMismoAnio_verdadero() {
+        Calendar calendar = Calendar.getInstance();
+        assertTrue(servicioPresupuestoBusiness.isMismoAnio(calendar));
+    }
+
+    public void testIsMismoAnio_falso() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.YEAR, 1);
+        assertFalse(servicioPresupuestoBusiness.isMismoAnio(calendar));
+    }
+
 }
